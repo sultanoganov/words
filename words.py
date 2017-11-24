939c9d5c1b3065f7a5bbcec99dd80e1f3ba93b15
unknown <oganov_S@12-11.iuca.loc>
2017-11-24 17:12:00 +0600

    first commit

---
+++ words.py
@@ -0,0 +1,34 @@
+words = {
+  'тетрадь': 'copybook',
+  'книга' : 'book',
+  'ручка' : 'pen',
+  'карандаш' : 'pencil',
+  'яблоко' : 'apple',
+  'цвет' : 'colour',
+}
+
+print(" "* 5,"Словарь")
+for key in words:
+  print(key, words[key])
+
+# for eng in 'Английский':
+#   print("Добавьте слово на")
+#   eng = input("Английском:")
+#   print("Напишите перевод на") 
+#   ru = input("Русском:")
+#   words[eng] = ru
+#   if eng != 'Английский':
+#     break
+
+
+while True:
+  print("Вы хотите добавить новое слово? да -1, нет - 2")
+  answer =  input("> ")
+  if answer == "1":
+    print("Добавьте слово на")
+    eng = input("Английском:")
+    print("Напишите перевод на") 
+    ru = input("Русском:")
+    words[eng] = ru
+  else:
+    break
